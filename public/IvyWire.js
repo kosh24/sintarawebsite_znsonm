0200f5dcd76000e713475dca525faa97fce4de54
Dmitry <kosh24@yandex.ru>
2024-09-29 16:17:15 +0500

    Initial commit

---
+++ public/IvyWire.js
@@ -0,0 +1,8 @@
+function myFunction(){
+	$("body>h2").animate({left: '-0%'}, 800);
+
+}
+function myFunction2(){
+	$(".logo").animate({left: '-0%'}, 800);
+}
+
---
+++ sinatra_znsonm.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'sendgrid-ruby'
+require 'sinatra/reloader'
+#include SendGrid
+
+get '/' do
+  erb :IvyWire
+end
+
+get '/projects' do
+  erb :IvyWireProjs
+end
+
+get '/ivory' do
+  erb :IvyWireBio
+end
+
+get '/contact' do
+  erb :IvyWireContact
+end
+
+
+
+
+not_found do
+  halt 404, 'page not found'
+end
